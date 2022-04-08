54afda17a34ca4be1bb353f301cf4bdc7396aecf
Sawardo <diego.sawady@gmail.com>
2022-04-08 13:35:13 -0400

    Pivote arreglado

--- scripts/pj1.gd
+++ scripts/pj1.gd
@@ -34,7 +34,8 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -1.4 * SPEED
 	
-	
+	#if Input.is_action_just_pressed("ataque"):
+	#	playback.travel("attack1")
 	
 	# Animation
 	if is_on_floor():
