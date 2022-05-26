e5bde92c288e7634db92fa2d02136cf98ce3c817
MiguelS4C <masanzanacar@gmail.com>
2022-05-26 17:31:09 -0400

    menu principal

---
+++ menu/Menu Inicio.gd
@@ -0,0 +1,14 @@
+extends MarginContainer
+
+onready var play= $PanelContainer/VBoxContainer/Play
+onready var quit= $PanelContainer/VBoxContainer/Quit
+
+func _ready():
+	play.connect("pressed",self, "_on_play_pressed")
+	quit.connect("pressed",self,"_on_quit_pressed")
+
+func _on_play_pressed():
+	get_tree().change_scene("res://.import/Pincipal.tscn")
+
+func _on_quit_pressed():
+	get_tree().quit()
