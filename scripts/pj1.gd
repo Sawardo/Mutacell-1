74b218af42e22af151f4dae3e139ced7a95c82ff
Sawardo <diego.sawady@gmail.com>
2022-05-28 14:04:40 -0400

    Sábado 28 de Mayo
    Tercer personaje (animaciones y animation Tree)

--- scripts/pj1.gd
+++ scripts/pj1.gd
@@ -39,8 +39,9 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -1.4 * SPEED
 	
-	#if Input.is_action_just_pressed("jump")
 	
+	
+# Change Character
 	if Input.is_action_just_pressed("change"):
 		if a == 1:
 			playback.travel("transida")
