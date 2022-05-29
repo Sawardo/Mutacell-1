3d0b0c93b3310b32de54508a36b20c3b552aab87
Sawardo <diego.sawady@gmail.com>
2022-05-29 00:24:08 -0400

    29/05 con Nadia-crack
    Finalmenche, el tercer personaje.
    Ataque 2 con dash, por ahí.

--- scripts/pj1.gd
+++ scripts/pj1.gd
@@ -6,7 +6,10 @@ var ACCELERATION = 500
 var GRAVITY = 400
 
 var velocity = Vector2()
-
+var specialattackposition = null
+var vectorcito
+var colisioncita
+var dasheando
 
 onready var areaLatigo = $Pivot/areaLatigo
 onready var sprite = $Pivot/Sprite
@@ -25,23 +28,28 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
-	var move_input = Input.get_axis("left", "right")
-	
-	
-	velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION)
-	
-	if not is_on_floor() and velocity.y <= 0:
-		velocity.y += GRAVITY * delta
+	if dasheando:
+		if get_slide_count() > 0:
+			dasheando = false
+	else:
 		
-	if not is_on_floor() and velocity.y >= 0 and velocity.y < 3000:
-		velocity.y += GRAVITY * delta
+		
+		var move_input = Input.get_axis("left", "right")
+		
+		
+		velocity.x = move_toward(velocity.x, move_input * SPEED, ACCELERATION)
+		
+		if not is_on_floor() and velocity.y <= 0:
+			velocity.y += GRAVITY * delta
+			
+		if not is_on_floor() and velocity.y >= 0 and velocity.y < 3000:
+			velocity.y += GRAVITY * delta
+		
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = -1.4 * SPEED
 	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = -1.4 * SPEED
 	
-	
-	
-# Change Character
+	# Change Character
 	if Input.is_action_just_pressed("change"):
 		if a == 1:
 			playback.travel("transida")
@@ -49,8 +57,25 @@ func _physics_process(delta):
 			a = 2
 		elif a == 2:
 			playback.travel("transgunida")
+			playback.travel("transarchvue")
+			a = 3
+		elif a == 3:
+			playback.travel("transarchida")
 			playback.travel("transvue")
 			a = 1
+	
+	
+	
+# Change Character
+	#if Input.is_action_just_pressed("change"):
+	#	if a == 1:
+	#		playback.travel("transida")
+	#		playback.travel("transgunvue")
+	#		a = 2
+	#	elif a == 2:
+	#		playback.travel("transgunida")
+	#		playback.travel("transvue")
+	#		a = 1
 
 
 
@@ -58,7 +83,18 @@ func _physics_process(delta):
 	if a == 1:
 		if Input.is_action_just_pressed("ataque"):
 			playback.travel("attack1")
+			specialattackposition = null
 			print("hem")
+		elif Input.is_action_just_pressed("sataque"):
+			playback.travel("attack1")
+			if specialattackposition:
+				vectorcito = (specialattackposition - global_position).normalized()
+				velocity = move_and_slide(vectorcito*1000)
+				dasheando = true
+				vectorcito = null
+				specialattackposition = null
+				
+			print("meh")
 		elif is_on_floor():
 			if abs(velocity.x) > 10:
 				playback.travel("run")
@@ -86,6 +122,20 @@ func _physics_process(delta):
 			else:
 				playback.travel("jumpgun")
 
+# Arch
+	if a == 3:
+		if is_on_floor():
+			if abs(velocity.x) > 10:
+				playback.travel("runarch")
+			else:
+				playback.travel("idlearch")
+		else:
+			if velocity.y > 0:
+				playback.travel("fallarch")
+			else:
+				playback.travel("jumparch")
+
+
 
 	if Input.is_action_pressed("right") and not Input.is_action_just_pressed("left"):
 		pivot.scale.x = 1
@@ -94,6 +144,7 @@ func _physics_process(delta):
 
 func _on_body_entered(body: Node):
 	if body.has_method("take_damage"):
+		specialattackposition = body.global_position
 		body.take_damage()
 
 
