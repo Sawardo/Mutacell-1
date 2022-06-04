72ab87a6479c7c6b8bc0c419fb0ff7f9c76d4d56
MiguelS4C <masanzanacar@gmail.com>
2022-06-04 00:27:44 -0400

    +del slime
    por lo menos ya se mueve

--- slime/slime.gd
+++ slime/slime.gd
@@ -2,8 +2,8 @@ extends KinematicBody2D
 
 onready var anim_tree= $AnimationTree
 onready var playback = anim_tree.get("parameters/playback")
-onready var anim_player= $AnimationPlayer
-onready var detection_area= $AreaDeteccion
+onready var detec_area= $AreaDeteccion
+
 
 
 var SPEED = 151
@@ -11,12 +11,12 @@ var ACCELERATION = 550
 var GRAVITY = 400
 
 var velocity = Vector2()
-var _target: KinematicBody = null
+var _target: Node2D = null
 
 
 func _ready():
 	anim_tree.active = true
-	detection_area.connect("body_entered", self, "on_body_entered")
+	detec_area.connect("body_entered", self, "_on_body_entered")
 
 func _physics_process(delta):
 	velocity= move_and_slide(velocity, Vector2.UP)
@@ -24,10 +24,21 @@ func _physics_process(delta):
 	var move_input= 0
 	
 	if _target:
-		move_input = sign(_target.global_position.x - global_position.x)
+		var diff= _target.global_position.x - global_position.x
+		move_input = sign(diff)
 		
 	velocity.x = move_toward(velocity.x, move_input* SPEED, ACCELERATION)
 	velocity.y += GRAVITY *delta
+		
+		
+	if _target and is_on_floor() and _target.global_position.y < global_position.y - 20:
+			velocity.y = -1 * SPEED
+	
+		
+	if abs(velocity.x) > 10:
+		playback.travel("walk")
+	else:
+		playback.travel("idle")
 
 #func take_damage(instigator: Node2D):
 	pass
