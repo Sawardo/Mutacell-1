558f5c6c3125aaecc81f17d928ee427c773f62b7
Sawardo <diego.sawady@gmail.com>
2022-06-17 12:30:02 -0400

    Revert "Merge branch 'main' of https://github.com/Sawardo/Mutacell-1"
    This reverts commit 7358c781f4995c7a57324ef26bbca19d3cd83015, reversing
    changes made to 13feae8b31c244c6ea8884c96d454c52ade6bcaa.

--- slime/slime.gd
+++ slime/slime.gd
@@ -1,49 +1,16 @@
 extends KinematicBody2D
 
-onready var anim_tree= $AnimationTree
-onready var playback = anim_tree.get("parameters/playback")
-onready var detec_area= $AreaDeteccion
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 
 
-
-var SPEED = 151
-var ACCELERATION = 550
-var GRAVITY = 400
-
-var velocity = Vector2()
-var _target: Node2D = null
-
-
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	anim_tree.active = true
-	detec_area.connect("body_entered", self, "_on_body_entered")
+	pass # Replace with function body.
 
-func _physics_process(delta):
-	velocity= move_and_slide(velocity, Vector2.UP)
-	
-	var move_input= 0
-	
-	if _target:
-		var diff= _target.global_position.x - global_position.x
-		move_input = sign(diff)
-		
-	velocity.x = move_toward(velocity.x, move_input* SPEED, ACCELERATION)
-	velocity.y += GRAVITY *delta
-		
-		
-	if _target and is_on_floor() and _target.global_position.y < global_position.y - 20:
-			velocity.y = -1 * SPEED
-	
-		
-	if abs(velocity.x) > 10:
-		playback.travel("walk")
-	else:
-		playback.travel("idle")
 
-#func take_damage(instigator: Node2D):
-	pass
-func take_damage():
-	playback.travel("hurt")
-
-func _on_body_entered(body: Node):
-	_target= body
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
